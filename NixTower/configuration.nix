af918249ed5e85b52567dca943e51c96f7148f01
LDexter <ldextermiller@gmail.com>
2023-11-09 06:51:08 +1000

    Discovered vim hiding in pre-generated config suggestions. Removed.

--- NixTower/configuration.nix
+++ NixTower/configuration.nix
@@ -100,7 +100,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
 
     # nyxt got kinda clunky and slow
